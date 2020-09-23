67e651987ff3c5ffd0d5e1d37bf4297aab2384fb
Vyctoria <vyctoriak@gmail.com>
2020-09-23 13:56:24 -0300

    corração de push, adding exercise 1011

--- Ex1011.java
+++ Ex1011.java
@@ -1,2 +1,35 @@
-package uri;public class Ex1011 {
+package uri;
+
+/*
+    PROBLEMA - Esfera
+    Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R).
+    A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
+    Dica: Ao utilizar a fórmula, procure usar (4/3.0) ou (4.0/3), pois algumas linguagens (dentre elas o C++),
+    assumem que o resultado da divisão entre dois inteiros é outro inteiro.
+
+    ENTRADA
+    O arquivo de entrada contém um valor de ponto flutuante (dupla precisão), correspondente ao raio da esfera.
+
+    SAÍDA
+    A saída deverá ser uma mensagem "VOLUME" conforme o exemplo fornecido abaixo, com um espaço antes e
+    um espaço depois da igualdade. O valor deverá ser apresentado com 3 casas após o ponto.
+
+ */
+
+import java.util.Scanner;
+
+public class Ex1011 {
+
+    public static void main(String[] args) {
+        double raio, volume, pi;
+        Scanner input = new Scanner(System.in);
+        raio = input.nextDouble();
+        input.close();
+        pi = 3.14159;
+        volume = (4/3.0) * pi * (raio * raio * raio);
+
+        System.out.printf("VOLUME = %.3f\n", volume );
+
+    }
+
 }
