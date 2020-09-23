79d9c86a1543ad04f018a104a3ac1558bc2433a6
Vyctoria <vyctoriak@gmail.com>
2020-09-23 02:36:41 -0300

    add exercise 1002

---
+++ Ex1002.java
@@ -0,0 +1,30 @@
+package uri;
+
+/* PROBLEMA
+  A fórmula para calcular a área de uma circunferência é: area = π . raio2.
+  Considerando para este problema que π = 3.14159:
+  - Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+  ENTRADA
+  A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+
+  SAÍDA
+  Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo,
+  com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double).
+  Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado,
+  caso contrário, você receberá "Presentation Error".
+
+*/
+
+import java.util.Scanner;
+
+public class Ex1002 {
+    public static void main(String[] args) {
+        double n = 3.14159;
+        Scanner leitor = new Scanner(System.in);
+        double raio = leitor.nextDouble();
+        leitor.close();
+        double area = n * (raio * raio);
+        System.out.println(String.format("A=%.4f", area));
+    }
+}
